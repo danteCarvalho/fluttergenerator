a33d5673aa075a4e95e936b535b5a4485e9d2cfa
Dante Carvalho Costa <dantec.c@hotmail.com>
2024-06-02 12:15:39 -0300

    gerando arquivos ao rodar

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,7 @@
+initDir=$(pwd)
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 appDir="$(cd "$scriptDir/.." && pwd)"
+cd $appDir
+./scripts/gerarArquivos.sh
+cd $initDir
 dart --no-sound-null-safety "$appDir"/lib/main.dart
